74102438702c1ed7af585045affe25055b2c8cf9
Christopher Justice <justcd@mail.broward.edu>
2017-07-03 13:29:03 -0400

    setup model

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,7 @@
 class Listing < ActiveRecord::Base
+	belongs_to :category
+	
   validates :title, presence: true
   validates :description, presence: true
+  validates :price, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,14 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140827203355) do
+ActiveRecord::Schema.define(version: 20170703172146) do
 
-  create_table "listings", force: true do |t|
-    t.string   "title"
-    t.text     "description"
-    t.decimal  "price"
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "listings", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.decimal "price"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer "category_id"
+    t.index ["category_id"], name: "index_listings_on_category_id"
   end
 
 end

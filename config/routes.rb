ce41802d1ca7d01eb3422ce49a0adfb75c32863e
Christopher Justice <justcd@mail.broward.edu>
2017-07-03 18:12:30 -0400

    you can now list and delete postings

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   root 'listings#index'
 
+  resources :categories, only:[:show]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
